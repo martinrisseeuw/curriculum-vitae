f80aa11aebbb5e5ee6eb5e6cbd436cb34ad4aef8
martinrisseeuw <hello@martinr.nl>
2017-10-12 12:11:14 +0200

    added favicon and title

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,14 +3,14 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'cv',
+    title: '📝 CV Martin Risseeuw',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'Nuxt.js project' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
     ]
   },
   css: [
